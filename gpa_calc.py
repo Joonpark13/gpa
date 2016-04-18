9c835d4ffbf0371a957ce7873fbda5c718ca3d0f
Joon Park <JoonPark2017@u.northwestern.edu>
2016-04-18 17:45:10 -0500

    Add initial parsing
    The parsing logic to separate the transcript lines into terms has been
    written. Actual parsing function yet to be implemented.

--- gpa_calc.py
+++ gpa_calc.py
@@ -1,7 +1,7 @@
 import os
 from nuapiclient import NorthwesternAPIClient
 
-client = NorthwesternAPIClient(os.env["NUAPI_KEY"])
+client = NorthwesternAPIClient(os.environ["NUAPI_KEY"])
 
 class Course:
 
@@ -19,7 +19,7 @@ courselist = []
 """ Parses the courses taken in a term
 
 :param term_id: term id used in the api, necessary for querying
-:param term_name: term name usedi in the api and transcript, stored in
+:param term_name: term name used in the api and transcript, stored in
     the Course object
 :param term_data: list of strings, each string is a line from the
     transcript within this term
@@ -38,9 +38,24 @@ def cum_gpa():
 # Interactive GAP calculation
 def main():
     # Get all terms
-    # If beginning of line matches a term, parse all lines following that term until the next term
-
-    pass
+    terms = [{"name":x['name'], "id":x["id"]} for x in client.terms()]
+    # If beginning of line matches a term, parse all lines following that term
+    # until the next term
+    term_lines = []
+    with open("transcript.txt", 'r') as file:
+        for line in file:
+            # Take the first two words of each line and compare to a term name
+            if len(line.split()) >= 2:
+                stripped_line = line.split()[0] + " " + line.split()[1]
+            else:
+                stripped_line = ""
+            # If a term line has been found, parse the current list, then reset
+            # the list
+            if stripped_line in [x["name"] for x in terms]:
+                parse_term(x["id"], stripped_line, term_lines)
+                term_lines = []
+            else:
+                term_lines.append(line)
 
     # User interaction
 
